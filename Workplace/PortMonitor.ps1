e620f97b36d0558d8bac1e512b7af37ba54807b0
Pixel Frame <pm421@live.com>
2020-05-27 22:02:42 +0800

    Add param and try/catch

--- Workplace/PortMonitor.ps1
+++ Workplace/PortMonitor.ps1
@@ -1,4 +1,27 @@
-if (!(Test-Path 'C:\PortMonitor')) { mkdir 'C:\PortMonitor' }
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $OutPath = $env:SystemDrive + '\?PortMonitor',
+
+    [Parameter()]
+    [int]
+    $LogLimit = 50
+)
+
+if (!(Test-Path $OutPath))
+{ 
+    try
+    {
+        mkdir $OutPath
+    }
+    catch
+    {
+        'Cannot create output path.'
+        $Error[0]
+        exit
+    }
+}
 
 $Ports = Get-NetTCPConnection | Select-Object -Property LocalPort, OwningProcess, State | Sort-Object -Property LocalPort
 
@@ -9,13 +32,23 @@ foreach ($Port in $Ports)
     $PortWithProcess += New-Object PSObject -Property @{'LocalPort' = $Port.LocalPort; 'PortState' = $Port.State; 'PID' = $Port.OwningProcess; 'ProcessName' = $Process.Name; 'CommandLine' = $Process.Path; 'Handles' = $Process.Handles; } 
 }
 
-$LogFile = 'C:\PortMonitor\PortUsage-' + (Get-Date -Format 'yyyyMMdd-HHmmss') + '.log'
+$LogFile = $OutPath + '\PortUsage-' + (Get-Date -Format 'yyyyMMdd-HHmmss') + '.log'
 
-'Ports:' | Out-File -FilePath $LogFile -Encoding utf8 -Force 
-$PortWithProcess | Format-Table -Property LocalPort, PID, ProcessName, PortState, CommandLine -AutoSize | Out-File -FilePath $LogFile -Encoding utf8 -Append
-'' | Out-File -FilePath $LogFile -Encoding utf8 -Append
-'Statics:' | Out-File -FilePath $LogFile -Encoding utf8 -Append
-$PortWithProcess | Group-Object -Property ProcessName | Out-File -FilePath $LogFile -Encoding utf8 -Append
+try
+{
+    'Ports:' | Out-File -FilePath $LogFile -Encoding utf8 -Force 
+    $PortWithProcess | Format-Table -Property LocalPort, PID, ProcessName, PortState, CommandLine -AutoSize | Out-File -FilePath $LogFile -Encoding utf8 -Append
+    '' | Out-File -FilePath $LogFile -Encoding utf8 -Append
+    'Statics:' | Out-File -FilePath $LogFile -Encoding utf8 -Append
+    $PortWithProcess | Group-Object -Property ProcessName | Out-File -FilePath $LogFile -Encoding utf8 -Append
 
-$Logs = Get-ChildItem 'C:\PortMonitor\PortUsage*'
-if($Logs.Count -gt 50) {Remove-Item $Logs[0] -Force}
+}
+catch
+{
+    'Unable to write to log file.'
+    $Error[0]
+    exit
+}
+
+$Logs = Get-ChildItem $OutPath'\PortUsage*'
+if ($Logs.Count -gt $LogLimit) { Remove-Item $Logs[0] -Force }
