487412eb896ad4916ebc67c16b2ad779f035d78e
Pixel Frame <pm421@live.com>
2020-07-01 23:02:00 +0800

    Update: AutoConfig refresh shortcuts

--- PsConfig/AutoConfig.ps1
+++ PsConfig/AutoConfig.ps1
@@ -1,5 +1,5 @@
 ##########################################################################
-##             PowerShell Auto Configuration Script v0.1                ##
+##             PowerShell Auto Configuration Script v1.0a               ##
 ##                                                          Pixel Frame ##
 ##                                                                      ##
 ##  1. [Plug-in] oh-my-posh & posh-git                                  ##
@@ -11,6 +11,9 @@
 ##    4.1 Color Tool                                                    ##
 ##    4.2 Sudo                                                          ##
 ##    4.3 Cascadia Fonts                                                ##
+##  5. [Config] Terminal Settings                                       ##
+##    5.1 Registry                                                      ##
+##    5.2 Refresh Windows PowerShell Shortcuts                          ##
 ##                                                                      ##
 ##########################################################################
 
@@ -371,6 +374,30 @@ foreach ($RegPath in $RegPaths)
     Set-ItemProperty -Path $RegPath -Name ScreenColors -Type DWORD -Value 0x00000001
     Set-ItemProperty -Path $RegPath -Name CodePage -Type DWORD -Value 65001
 }
+
+# Refresh Shortcuts
+
+$WshShell = New-Object -ComObject WScript.Shell
+
+$Shortcut = $env:USERPROFILE + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell_New.lnk'
+$WshShortcut = $WshShell.CreateShortcut($Shortcut)
+$WshShortcut.TargetPath = '%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe'
+$WshShortcut.Description = 'Performs object-based (command-line) functions'
+$WshShortcut.IconLocation = '%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe,0'
+$WshShortcut.WorkingDirectory = '%HOMEDRIVE%%HOMEPATH%'
+$WshShortcut.Save()
+Remove-Item -Path "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk"
+Rename-Item -Path "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell_New.lnk" -NewName "Windows PowerShell.lnk"
+
+$Shortcut = $env:USERPROFILE + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell (x86)_New.lnk'
+$WshShortcut = $WshShell.CreateShortcut($Shortcut)
+$WshShortcut.TargetPath = '%SystemRoot%\syswow64\WindowsPowerShell\v1.0\powershell.exe'
+$WshShortcut.Description = 'Performs object-based (command-line) functions'
+$WshShortcut.IconLocation = '%SystemRoot%\syswow64\WindowsPowerShell\v1.0\powershell.exe,0'
+$WshShortcut.WorkingDirectory = '%HOMEDRIVE%%HOMEPATH%'
+$WshShortcut.Save()
+Remove-Item -Path "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell (x86).lnk"
+Rename-Item -Path "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell (x86)_New.lnk" -NewName "Windows PowerShell (x86).lnk"
 
 Write-Host 'Execution Finished'
 Write-Host 'Restart Your Console Host and Check the Result'
