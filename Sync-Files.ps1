20739aed4db6369b6820f1f1101e4c10696a67dc
Pixel Frame <pm421@live.com>
2020-08-29 21:56:47 +0800

    Add: UTFlizer

--- Sync-Files.ps1
+++ Sync-Files.ps1
@@ -9,6 +9,43 @@ param (
     $DestPath
 )
 
+function SyncFolder
+{
+    param (
+        [string] $SourceFolder,
+        [string] $DestFolder
+    )
+    
+
+}
+
+function SyncFile
+{
+    param (
+        [string] $SourceFile,
+        [string] $DestFile
+    )
+    if (Test-Path $DestFile)
+    {
+        $SourceHash = Get-FileHash $SourceFile
+        $DestHash = Get-FileHash $DestFile
+        if ($DestHash -ne $SourceHash)
+        {
+            Copy-Item $SourceFile $DestFile -Force
+            "Copied $Source"
+        }
+        else
+        {
+            "Skipped $Source"
+        }
+    }
+    else
+    {
+        Copy-Item $SourceFile $DestFile -Force
+        "Copied $Source"
+    }
+}
+
 $SourceFiles = Get-ChildItem -Path $SourcePath
 $DestPath = Get-ChildItem -Path $DestPath
 
