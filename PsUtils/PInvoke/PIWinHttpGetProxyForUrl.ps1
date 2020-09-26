f383354831c520fed42d168137d0f403b993cf5b
Pixel Frame <pm421@live.com>
2020-09-26 13:15:14 +0800

    Migrate PInvoke WinHttp to PowerShell

---
+++ PsUtils/PInvoke/PIWinHttpGetProxyForUrl.ps1
@@ -0,0 +1,114 @@
+[CmdletBinding()]
+param (
+    [Parameter(ValueFromPipeline)]
+    [string]
+    $Url = 'http://example.com/'
+)
+
+$Win32CallDef = @'
+using System;
+using System.Runtime.InteropServices;
+
+    public class WinHttp
+    {
+        [DllImport("winhttp.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr WinHttpOpen(
+            string pwszAgentW,
+            AccessType dwAccessType,
+            string pwszProxyW,
+            string pwszProxyBypassW,
+            OpenFlag dwFlags);
+
+        [DllImport("winhttp.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool WinHttpGetProxyForUrl(
+            IntPtr hSession,
+            string lpcwszUrl,
+            ref WINHTTP_AUTOPROXY_OPTIONS pAutoProxyOptions,
+            ref WINHTTP_PROXY_INFO pProxyInfo);
+
+        [DllImport("winhttp.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool WinHttpGetDefaultProxyConfiguration(ref WINHTTP_PROXY_INFO pProxyInfo);
+
+        [DllImport("winhttp.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool WinHttpCloseHandle(IntPtr hInternet);
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct WINHTTP_AUTOPROXY_OPTIONS
+    {
+        public AutoProxyFlag dwFlags;
+        public AutoDetectFlag dwAutoDetectFlags;
+        public string lpszAutoConfigUrl;
+        public IntPtr lpvReserved;
+        public int dwReserved;
+        public bool fAutoLogonIfChallenged;
+    }
+
+    [Flags]
+    public enum AccessType
+    {
+        WINHTTP_ACCESS_TYPE_NO_PROXY = 0x1,
+        WINHTTP_ACCESS_TYPE_DEFAULT_PROXY = 0x0,
+        WINHTTP_ACCESS_TYPE_NAMED_PROXY = 0x3,
+        WINHTTP_ACCESS_TYPE_AUTOMATIC_PROXY = 0x4
+    }
+
+    [Flags]
+    public enum OpenFlag
+    {
+        WINHTTP_FLAG_ASYNC = 0x10000000,
+        WINHTTP_FLAG_SECURE_DEFAULTS = 0x30000000
+    }
+
+    [Flags]
+    public enum AutoProxyFlag
+    {
+        WINHTTP_AUTOPROXY_AUTO_DETECT = 0x00000001,
+        WINHTTP_AUTOPROXY_CONFIG_URL = 0x00000002,
+        WINHTTP_AUTOPROXY_HOST_KEEPCASE = 0x00000004,
+        WINHTTP_AUTOPROXY_HOST_LOWERCASE = 0x00000008,
+        WINHTTP_AUTOPROXY_ALLOW_AUTOCONFIG = 0x00000100,
+        WINHTTP_AUTOPROXY_ALLOW_STATIC = 0x00000200,
+        WINHTTP_AUTOPROXY_ALLOW_CM = 0x00000400,
+        WINHTTP_AUTOPROXY_RUN_INPROCESS = 0x00010000,
+        WINHTTP_AUTOPROXY_RUN_OUTPROCESS_ONLY = 0x00020000,
+        WINHTTP_AUTOPROXY_NO_DIRECTACCESS = 0x00040000,
+        WINHTTP_AUTOPROXY_NO_CACHE_CLIENT = 0x00080000,
+        WINHTTP_AUTOPROXY_NO_CACHE_SVC = 0x00100000,
+    }
+
+    [Flags]
+    public enum AutoDetectFlag
+    {
+        WINHTTP_AUTO_DETECT_TYPE_DHCP = 0x00000001,
+        WINHTTP_AUTO_DETECT_TYPE_DNS_A = 0x00000002
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct WINHTTP_PROXY_INFO
+    {
+        public AccessType dwAccessType;
+        public string lpszProxy;
+        public string lpszProxyBypass;
+    }
+'@
+
+Add-Type -TypeDefinition $Win32CallDef -ErrorAction SilentlyContinue
+
+$SessionHandle = [WinHttp]::WinHttpOpen("PINVOKE WINHTTP CLIENT/1.0", [AccessType]::WINHTTP_ACCESS_TYPE_NO_PROXY, "", "", 0);
+
+$AutoProxyOptions = New-Object WINHTTP_AUTOPROXY_OPTIONS
+$AutoProxyOptions.dwFlags = [AutoProxyFlag]::WINHTTP_AUTOPROXY_AUTO_DETECT
+$AutoProxyOptions.dwAutoDetectFlags = [AutoDetectFlag]::WINHTTP_AUTO_DETECT_TYPE_DHCP -bor [AutoDetectFlag]::WINHTTP_AUTO_DETECT_TYPE_DNS_A
+$AutoProxyOptions.fAutoLogonIfChallenged = $true
+
+$ProxyInfo = New-Object WINHTTP_PROXY_INFO
+
+if([WinHttp]::WinHttpGetProxyForUrl($SessionHandle, $Url, [ref] $AutoProxyOptions, [ref] $ProxyInfo))
+{
+    $ProxyInfo | Format-List
+}
+else {
+    $ErrorCode = [System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
+    "WinHttpGetProxyForUrl Auto Detect Failed. Code: $ErrorCode"
+}
