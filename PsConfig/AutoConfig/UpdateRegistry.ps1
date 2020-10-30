d6b47e67a75a0754b42a8113a01ee2a9c83fe559
Pixel Frame <pm421@live.com>
2020-10-30 09:13:21 +0800

    Add pwsh-preview

--- PsConfig/AutoConfig/UpdateRegistry.ps1
+++ PsConfig/AutoConfig/UpdateRegistry.ps1
@@ -1,12 +1,13 @@
 [string[]] $RegPaths += 'HKCU:\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe'
 $RegPaths += 'HKCU:\Console\%SystemRoot%_SysWOW64_WindowsPowerShell_v1.0_powershell.exe'
 $RegPaths += 'HKCU:\Console\C:_Program Files_PowerShell_7_pwsh.exe'
+$RegPaths += 'HKCU:\Console\C:_Program Files_PowerShell_7-preview_pwsh.exe'
 foreach ($RegPath in $RegPaths)
 { 
     if (!(Test-Path $RegPath)) { New-Item $RegPath | Out-Null }
 
     Set-ItemProperty -Path $RegPath -Name FaceName -Type STRING -Value "Cascadia Code PL"
     Set-ItemProperty -Path $RegPath -Name FontSize -Type DWORD -Value 0x00120000
-    Set-ItemProperty -Path $RegPath -Name ScreenColors -Type DWORD -Value 0x00000001
+    Set-ItemProperty -Path $RegPath -Name ScreenColors -Type DWORD -Value 0x00000009
     Set-ItemProperty -Path $RegPath -Name CodePage -Type DWORD -Value 65001
 }
