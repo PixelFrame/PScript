eae524eb9497a5b0e3d6cfc5e80924d9fadf8933
Pixel Frame <pm421@live.com>
2020-11-14 20:57:59 +0800

    Update Export-NetshTraces

--- PsTrace/Export-NetshTraces.ps1
+++ PsTrace/Export-NetshTraces.ps1
@@ -1,26 +1,52 @@
-$WppTraceReg = 'HKLM:\SYSTEM\CurrentControlSet\Control\NetDiagFx\Microsoft\HostDLLs\WPPTrace\HelperClasses\'
+$WppTraceReg = 'HKLM:\SYSTEM\CurrentControlSet\Control\NetDiagFx\Microsoft\HostDLLs\*\HelperClasses\*'
 $Scenarios = Get-ChildItem $WppTraceReg
 
-$ScenarioObjects = @()
+$ScenarioObjects = @{}
 foreach ($Scenario in $Scenarios)
 {
     $ScenarioName = $Scenario.Name.Substring($Scenario.Name.LastIndexOf('\') + 1)
     $Providers = $Scenario.OpenSubKey('Providers')
-    $ProviderGuids = $Providers.GetSubKeyNames()
-    $ProviderObjects = @()
-    foreach ($ProviderGuid in $ProviderGuids)
+    if ($null -ne $Providers)
     {
-        $Provider = $Providers.OpenSubKey($ProviderGuid)
-        $ProviderObjects += [PSCustomObject]@{
-            Guid     = $ProviderGuid;
-            Keywords = $Provider.GetValue('Keywords');
-            Level    = $Provider.GetValue('Level');
-            Name     = $Provider.GetValue('Name')
+        $ProviderGuids = $Providers.GetSubKeyNames()
+        $ProviderObjects = New-Object System.Collections.ArrayList
+        foreach ($ProviderGuid in $ProviderGuids)
+        {
+            $Provider = $Providers.OpenSubKey($ProviderGuid)
+            $ProviderObjects.Add([PSCustomObject]@{
+                    Guid     = $ProviderGuid;
+                    Keywords = $Provider.GetValue('Keywords');
+                    Level    = $Provider.GetValue('Level');
+                    Name     = $Provider.GetValue('Name')
+                }) | Out-Null
         }
+        $ScenarioObjects[$ScenarioName] = $ProviderObjects
     }
-    $ScenarioObjects += [PSCustomObject]@{
-        Name      = $ScenarioName;
-        Providers = $ProviderObjects
+    else
+    {
+        $ScenarioObjects[$ScenarioName] = @()
+        "No provider for this scenario"
     }
 }
-$ScenarioObjects
+foreach ($Scenario in $Scenarios)
+{
+    $ScenarioName = $Scenario.Name.Substring($Scenario.Name.LastIndexOf('\') + 1)
+    $Dependencies = $Scenario.OpenSubKey('Dependencies')
+    if ($null -ne $Dependencies)
+    {
+        $DependencyNames = $Dependencies.GetSubKeyNames()
+        foreach ($DependencyName in $DependencyNames)
+        {
+            $ScenarioObjects[$ScenarioName].AddRange($ScenarioObjects[$DependencyName]) | Out-Null
+        }
+    }
+    else
+    {
+        "No dependency for this scenario"
+    }
+}
+
+$ScenarioObjects.GetEnumerator() | ForEach-Object {
+    'Scenario: ' + $_.Name
+    $_.Value | fl
+}
