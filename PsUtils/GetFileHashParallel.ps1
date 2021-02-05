fc517866ac34a3d9b540a67d016290df7eae7536
Pixel Frame <pm421@live.com>
2021-02-05 20:48:17 +0800

    Multithreading & Show Progress

--- PsUtils/GetFileHashParallel.ps1
+++ PsUtils/GetFileHashParallel.ps1
@@ -16,13 +16,27 @@ param (
     $ThreadCount = 8
 )
 
-$Result = [System.Collections.Generic.SortedDictionary[string, string]]@{}
 $Files = Get-ChildItem -Path $Path -File -Recurse
+$Total = $Files.Length
+$Result = New-Object 'System.Collections.Concurrent.ConcurrentDictionary[string, string]' -ArgumentList @($ThreadCount, $Total)
 
-$Files | ForEach-Object -Parallel {
+"Total File Count: $Total"
+
+$Job = $Files | ForEach-Object -Parallel {
+    $ResultCopy = $($using:Result)
     $File = $_.FullName.Replace('[', '`[').Replace(']', '`]')
     $Hash = Get-FileHash -Path $File -Algorithm SHA512
-    $($using:Result).Add($Hash.Path, $Hash.Hash)
-} -ThrottleLimit $ThreadCount
+    $ResultCopy[$File] = $Hash.Hash
+} -ThrottleLimit $ThreadCount -AsJob
 
-$Result.GetEnumerator() | Select-Object @{N = 'Path'; E = { $_.Key } }, @{N = 'SHA512'; E = { $_.Value } } | Export-Csv -Path $Output
+while ($Job.State -eq 'Running')
+{
+    $Percent = $Result.Count / $Total * 100
+    Write-Progress -Activity "Calculating File Hash using $ThreadCount Threads" -PercentComplete $Percent -Status "Hashed File: $($Result.Count) / $Total"
+
+    Start-Sleep -Seconds 0.1
+}
+
+"Hash Completed"
+"Writing Output"
+$Result.GetEnumerator() | Sort-Object -Property Key | Select-Object @{N = 'Path'; E = { $_.Key } }, @{N = 'SHA512'; E = { $_.Value } } | Export-Csv -Path $Output
