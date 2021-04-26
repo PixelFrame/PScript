673da63a9ad9a5117e39a6edbea6daccb96f97bb
Pixel Frame <pm421@live.com>
2021-04-26 10:38:15 +0800

    Add param to IPsec GP

--- Workplace/IPsec/IPsecGp.ps1
+++ Workplace/IPsec/IPsecGp.ps1
@@ -1,39 +1,51 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $CertAuthority,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    $PolicyStore
+)
+
+
 # MAIN MODE
 # Encryption: AES256
 # DHGroup: DH19
 # Integrity: SHA256
 $MMProp = New-NetIPsecMainModeCryptoProposal -Encryption AES256 -KeyExchange DH19 -Hash SHA256
-$MMSet = New-NetIPsecMainModeCryptoSet -DisplayName "MM Set AES256-DH19-SHA256" -Proposal $MMProp -PolicyStore 'shlth.vlab\SHL Firewall'
-New-NetIPsecMainModeRule -DisplayName "MM Rule AES256-DH19-SHA256" -MainModeCryptoSet $MMSet.Name -PolicyStore 'shlth.vlab\SHL Firewall'
+$MMSet = New-NetIPsecMainModeCryptoSet -DisplayName "MM Set AES256-DH19-SHA256" -Proposal $MMProp -PolicyStore $PolicyStore
+New-NetIPsecMainModeRule -DisplayName "MM Rule AES256-DH19-SHA256" -MainModeCryptoSet $MMSet.Name -PolicyStore $PolicyStore
 
 # Phase 1 Auth
 # Machine Cert
-$MachineCertProp = New-NetIPsecAuthProposal -Machine -Cert -Authority "DC=vlab, DC=shlth, CN=shlth-SHL-PDC-CA" -AuthorityType Root
-$Phase1Auth = New-NetIPsecPhase1AuthSet -DisplayName "SHLTH Machine Cert Auth" -Proposal $MachineCertProp -PolicyStore 'shlth.vlab\SHL Firewall'
+$MachineCertProp = New-NetIPsecAuthProposal -Machine -Cert -Authority $CertAuthority -AuthorityType Root
+$Phase1Auth = New-NetIPsecPhase1AuthSet -DisplayName "ARG Machine Cert Auth" -Proposal $MachineCertProp -PolicyStore $PolicyStore
 
 # Phase 2 Auth
 # User Cert + Kerberos
-$UserCertProp = New-NetIPsecAuthProposal -User -Cert -Authority "DC=vlab, DC=shlth, CN=shlth-SHL-PDC-CA" -AuthorityType Root
+$UserCertProp = New-NetIPsecAuthProposal -User -Cert -Authority $CertAuthority -AuthorityType Root
 $UserKerbProp = New-NetIPsecAuthProposal -User -Kerberos
-$Phase2Auth = New-NetIPsecPhase2AuthSet -DisplayName "SHLTH User Cert+Kerberos Auth" -Proposal $UserCertProp, $UserKerbProp -PolicyStore 'shlth.vlab\SHL Firewall'
+$Phase2Auth = New-NetIPsecPhase2AuthSet -DisplayName "ARG User Cert+Kerberos Auth" -Proposal $UserCertProp, $UserKerbProp -PolicyStore $PolicyStore
 
 # QUICK MODE
 # Encryption: AES256
 # Encap: ESP
 # Integrity: SHA256
 $QMProp = New-NetIPsecQuickModeCryptoProposal -Encryption AES256 -Encapsulation ESP -ESPHash SHA256
-$QMSet = New-NetIPsecQuickModeCryptoSet -DisplayName "QM Set AES256-ESP-SHA256" -Proposal $QMProp -PolicyStore 'shlth.vlab\SHL Firewall'
+$QMSet = New-NetIPsecQuickModeCryptoSet -DisplayName "QM Set AES256-ESP-SHA256" -Proposal $QMProp -PolicyStore $PolicyStore
 
-New-NetIPsecRule -DisplayName "VLAB Internal HTTP IPsec Outbound" -InboundSecurity Require -OutboundSecurity Require `
+New-NetIPsecRule -DisplayName "ARG-IPsec WinRM (TCP-Out)" -Name "IPsec-WinRM-Out-TCP" -InboundSecurity Require -OutboundSecurity Require `
     -Phase1AuthSet $Phase1Auth.Name -Phase2AuthSet $Phase2Auth.Name -QuickModeCryptoSet $QMSet.Name `
-    -Protocol TCP -RemoteAddress 192.168.10.0/24 -RemotePort 443, 80 `
+    -Protocol TCP -RemoteAddress 192.168.10.0/24 -RemotePort 5985 `
     -Mode Transport `
     -Profile Domain `
-    -PolicyStore 'shlth.vlab\SHL Firewall'
+    -PolicyStore $PolicyStore
 
-New-NetIPsecRule -DisplayName "VLAB Internal HTTP IPsec Inbound" -InboundSecurity Require -OutboundSecurity Require `
+New-NetIPsecRule -DisplayName "ARG-IPsec WinRM (TCP-In)" -Name "IPsec-WinRM-In-TCP" -InboundSecurity Require -OutboundSecurity Require `
     -Phase1AuthSet $Phase1Auth.Name -Phase2AuthSet $Phase2Auth.Name -QuickModeCryptoSet $QMSet.Name `
-    -Protocol TCP -LocalAddress 192.168.10.0/24 -LocalPort 443, 80 `
+    -Protocol TCP -LocalAddress 192.168.10.0/24 -LocalPort 5985 `
     -Mode Transport `
     -Profile Domain `
-    -PolicyStore 'shlth.vlab\SHL Firewall'
+    -PolicyStore $PolicyStore
