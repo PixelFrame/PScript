2d6af4c14967ee16ca92722d82082f60b06e6348
Pixel Frame <pm421@live.com>
2021-08-09 17:01:48 +0800

    Update OpenSSH version

--- PsUtils/Install-OpenSshServer.ps1
+++ PsUtils/Install-OpenSshServer.ps1
@@ -1,6 +1,6 @@
 #Requires -RunAsAdministrator
 
-Invoke-WebRequest -Uri 'https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.1.0.0p1-Beta/OpenSSH-Win64.zip' -UseBasicParsing -OutFile $env:USERPROFILE\OpenSSH-Win64.zip
+Invoke-WebRequest -Uri 'https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.6.0.0p1-Beta/OpenSSH-Win64.zip' -UseBasicParsing -OutFile $env:USERPROFILE\OpenSSH-Win64.zip
 Expand-Archive -Path $env:USERPROFILE\OpenSSH-Win64.zip -DestinationPath $env:ProgramFiles\
 & $env:ProgramFiles\OpenSSH-Win64\Install-Sshd.ps1
 Import-Module $env:ProgramFiles\OpenSSH-Win64\OpenSSHUtils.psm1 -Scope Global
