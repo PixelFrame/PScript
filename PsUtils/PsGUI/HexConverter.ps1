588db4be10ed80544773f803a08dc84af387031e
Pixel Frame <pm421@live.com>
2021-11-28 20:16:45 +0800

    Add DNS types for HexConverter

--- PsUtils/PsGUI/HexConverter.ps1
+++ PsUtils/PsGUI/HexConverter.ps1
@@ -1,5 +1,6 @@
 Add-Type -AssemblyName System.Windows.Forms
 
+#REGION Widget Callbacks
 function onASCII
 {
     $RegexPattern = New-Object regex '[\\\r\n\t, ]|0x'
@@ -23,12 +24,83 @@ function onUnicode
     $textBox2.Text = DoConvert 'Unicode' $HexString
 }
 
+function onDnsA
+{
+    $RegexPattern = New-Object regex '[\\\r\n\t, ]|0x'
+    $HexString = $RegexPattern.Replace($textBox1.Text, '')
+    $HexBytes = StringToByteArray($HexString)
+
+    if ($HexBytes.Count -ne 4)
+    {
+        $Output = "Invalid Hex string"
+    }
+    else
+    {
+        $Output = $HexBytes -join '.'
+    }
+
+    $textBox2.Text = $Output
+}
+
+function onDnsPTR
+{
+    $RegexPattern = New-Object regex '[\\\r\n\t, ]|0x'
+    $HexString = $RegexPattern.Replace($textBox1.Text, '')
+    $HexBytes = StringToByteArray($HexString)
+
+    $FullLength = [int] $HexBytes[0]
+    $SegCount = [int] $HexBytes[1]
+    $Index = 2
+    $Output = @" 
+Length: $FullLength
+Segment Count: $SegCount
+Segments:
+"@
+
+    while ($Index -lt $FullLength) 
+    {
+        $SegLength = [int] $HexBytes[$Index]
+        $SegBytes = New-Object 'byte[]' -ArgumentList $SegLength
+        [Array]::Copy($HexBytes, $Index + 1, $SegBytes, 0, $SegLength)
+        $Seg = [System.Text.Encoding]::UTF8.GetString($SegBytes)
+        $Output += "`r`n  ($SegLength)$Seg"
+        $Index += ($SegLength + 1)
+        # Write-Host "[DBG] CURRENT INDEX $Index"
+    }
+
+    $textBox2.Text = $Output
+}
+
+function onDnsCNAME
+{
+    $RegexPattern = New-Object regex '[\\\r\n\t, ]|0x'
+    $HexString = $RegexPattern.Replace($textBox1.Text, '')
+    $HexBytes = StringToByteArray($HexString)
+
+    $FullLength = [int] $HexBytes[0]
+    $NameBytes = New-Object 'byte[]' -ArgumentList $FullLength
+    [Array]::Copy($HexBytes, 1, $NameBytes, 0, $FullLength)
+    $Name = [System.Text.Encoding]::UTF8.GetString($NameBytes)
+    $Output = @" 
+Length: $FullLength
+Name: $Name
+"@
+
+    $textBox2.Text = $Output
+}
+
 function onWordWarp
 {
     $textBox2.WordWrap = $checkBox.Checked;
-
 }
 
+function UpdateSelected
+{
+    $label.Text = "Selected: $($textBox2.SelectionLength)";
+}
+#ENDREGION
+
+#REGION Auxiliary Functions
 function SplitOnNull
 {
     [CmdletBinding()]
@@ -76,7 +148,7 @@ function DoConvert
         [string]
         $HexString
     )
-    Write-Host "[DBG] $HexString"
+    # Write-Host "[DBG] $HexString"
     $HexBytes = StringToByteArray($HexString)
     if ($null -eq $HexBytes)
     {
@@ -130,13 +202,9 @@ function GetHexVal
     else { $val -= 87 }
     return $val;
 }
+#ENDREGION
 
-function UpdateSelected {
-    param (
-    )
-    $label.Text = "Selected: $($textBox2.SelectionLength)";
-}
-
+#REGION WinForm Design
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $Form = New-Object System.Windows.Forms.Form
@@ -146,9 +214,12 @@ $groupBox1 = New-Object System.Windows.Forms.GroupBox
 $groupBox2 = New-Object System.Windows.Forms.GroupBox
 $textBox1 = New-Object System.Windows.Forms.TextBox
 $textBox2 = New-Object System.Windows.Forms.TextBox
-$button1 = New-Object System.Windows.Forms.Button
-$button2 = New-Object System.Windows.Forms.Button
-$button3 = New-Object System.Windows.Forms.Button
+$buttonAscii = New-Object System.Windows.Forms.Button
+$buttonUtf8 = New-Object System.Windows.Forms.Button
+$buttonUnicode = New-Object System.Windows.Forms.Button
+$buttonDnsA = New-Object System.Windows.Forms.Button
+$buttonDnsPTR = New-Object System.Windows.Forms.Button
+$buttonDnsCNAME = New-Object System.Windows.Forms.Button
 $checkBox = New-Object System.Windows.Forms.CheckBox
 $label = New-Object System.Windows.Forms.Label
 
@@ -169,20 +240,24 @@ $tableLayoutPanel1.Name = "tableLayoutPanel1";
 $tableLayoutPanel1.RowCount = 3;
 $tableLayoutPanel1.RowStyles.Add((New-Object System.Windows.Forms.RowStyle -ArgumentList @([System.Windows.Forms.SizeType]::Percent, 50)));
 $tableLayoutPanel1.RowStyles.Add((New-Object System.Windows.Forms.RowStyle -ArgumentList @([System.Windows.Forms.SizeType]::Percent, 50)));
-$tableLayoutPanel1.RowStyles.Add((New-Object System.Windows.Forms.RowStyle -ArgumentList @([System.Windows.Forms.SizeType]::Absolute, 30)));
+$tableLayoutPanel1.RowStyles.Add((New-Object System.Windows.Forms.RowStyle -ArgumentList @([System.Windows.Forms.SizeType]::Absolute, 70)));
 $tableLayoutPanel1.Size = New-Object System.Drawing.Size -ArgumentList @(800, 450);
 
 $tableLayoutPanel2.ColumnCount = 5;
-$tableLayoutPanel2.ColumnStyles.Add((New-Object System.Windows.Forms.ColumnStyle -ArgumentList @([System.Windows.Forms.SizeType]::Absolute, 80)));
-$tableLayoutPanel2.ColumnStyles.Add((New-Object System.Windows.Forms.ColumnStyle -ArgumentList @([System.Windows.Forms.SizeType]::Absolute, 80)));
-$tableLayoutPanel2.ColumnStyles.Add((New-Object System.Windows.Forms.ColumnStyle -ArgumentList @([System.Windows.Forms.SizeType]::Absolute, 80)));
+$tableLayoutPanel2.ColumnStyles.Add((New-Object System.Windows.Forms.ColumnStyle -ArgumentList @([System.Windows.Forms.SizeType]::Absolute, 100)));
+$tableLayoutPanel2.ColumnStyles.Add((New-Object System.Windows.Forms.ColumnStyle -ArgumentList @([System.Windows.Forms.SizeType]::Absolute, 100)));
+$tableLayoutPanel2.ColumnStyles.Add((New-Object System.Windows.Forms.ColumnStyle -ArgumentList @([System.Windows.Forms.SizeType]::Absolute, 100)));
 $tableLayoutPanel2.ColumnStyles.Add((New-Object System.Windows.Forms.ColumnStyle -ArgumentList @([System.Windows.Forms.SizeType]::Absolute, 100)));
 $tableLayoutPanel2.ColumnStyles.Add((New-Object System.Windows.Forms.ColumnStyle -ArgumentList @([System.Windows.Forms.SizeType]::Absolute, 300)));
-$tableLayoutPanel2.RowCount = 1;
-$tableLayoutPanel2.RowStyles.Add((New-Object System.Windows.Forms.RowStyle -ArgumentList @([System.Windows.Forms.SizeType]::Percent, 100)));
-$tableLayoutPanel2.Controls.Add($button1, 0, 0);
-$tableLayoutPanel2.Controls.Add($button2, 1, 0);
-$tableLayoutPanel2.Controls.Add($button3, 2, 0);
+$tableLayoutPanel2.RowCount = 2;
+$tableLayoutPanel2.RowStyles.Add((New-Object System.Windows.Forms.RowStyle -ArgumentList @([System.Windows.Forms.SizeType]::Percent, 50)));
+$tableLayoutPanel2.RowStyles.Add((New-Object System.Windows.Forms.RowStyle -ArgumentList @([System.Windows.Forms.SizeType]::Percent, 50)));
+$tableLayoutPanel2.Controls.Add($buttonAscii, 0, 0);
+$tableLayoutPanel2.Controls.Add($buttonUtf8, 1, 0);
+$tableLayoutPanel2.Controls.Add($buttonUnicode, 2, 0);
+$tableLayoutPanel2.Controls.Add($buttonDnsA, 0, 1);
+$tableLayoutPanel2.Controls.Add($buttonDnsPTR, 1, 1);
+$tableLayoutPanel2.Controls.Add($buttonDnsCNAME, 2, 1);
 $tableLayoutPanel2.Controls.Add($checkBox, 3, 0);
 $tableLayoutPanel2.Controls.Add($label, 4, 0);
 $tableLayoutPanel2.Dock = [System.Windows.Forms.DockStyle]::Fill;
@@ -191,60 +266,67 @@ $tableLayoutPanel2.Name = "tableLayoutPanel2";
 
 $groupBox1.Controls.Add($textBox1);
 $groupBox1.Dock = [System.Windows.Forms.DockStyle]::Fill;
-$groupBox1.Location = New-Object System.Drawing.Point -ArgumentList @(3, 3);
 $groupBox1.Name = "groupBox1";
-$groupBox1.Size = New-Object System.Drawing.Size -ArgumentList @(794, 204);
 $groupBox1.TabStop = $false;
 $groupBox1.Text = "HEX";
 
 $groupBox2.Controls.Add($textBox2);
 $groupBox2.Dock = [System.Windows.Forms.DockStyle]::Fill;
-$groupBox2.Location = New-Object System.Drawing.Point -ArgumentList @(3, 213);
 $groupBox2.Name = "groupBox2";
-$groupBox2.Size = New-Object System.Drawing.Size -ArgumentList @(794, 204);
 $groupBox2.TabStop = $false;
 $groupBox2.Text = "STRING";
 
 $textBox1.Dock = [System.Windows.Forms.DockStyle]::Fill;
-$textBox1.Location = New-Object System.Drawing.Point -ArgumentList @(3, 19);
 $textBox1.Multiline = $true;
 $textBox1.Name = "textBox1";
-$textBox1.Size = New-Object System.Drawing.Size -ArgumentList @(788, 182);
 $textBox1.ScrollBars = [System.Windows.Forms.ScrollBars]::Vertical;
 $textBox1.Font = New-Object System.Drawing.Font('Consolas', 10);
 
 $textBox2.Dock = [System.Windows.Forms.DockStyle]::Fill;
-$textBox2.Location = New-Object System.Drawing.Point -ArgumentList @(3, 19);
 $textBox2.Multiline = $true;
 $textBox2.Name = "textBox2";
 $textBox2.ReadOnly = $true;
-$textBox2.Size = New-Object System.Drawing.Size -ArgumentList @(788, 182);
 $textBox2.ScrollBars = [System.Windows.Forms.ScrollBars]::Both;
 $textBox2.WordWrap = $false;
 $textBox2.Font = New-Object System.Drawing.Font('Consolas', 10);
 $textBox2.HideSelection = $false;
 $textBox2.Add_Click( { UpdateSelected } );
 
-$button1.Location = New-Object System.Drawing.Point -ArgumentList @(3, 423);
-$button1.Name = "button1";
-$button1.Size = New-Object System.Drawing.Size -ArgumentList @(75, 23);
-$button1.Text = "ASCII";
-$button1.UseVisualStyleBackColor = $true;
-$button1.Add_Click( { onASCII });
+$buttonAscii.Dock = [System.Windows.Forms.DockStyle]::Fill;
+$buttonAscii.Name = "buttonAscii";
+$buttonAscii.Text = "ASCII";
+$buttonAscii.UseVisualStyleBackColor = $true;
+$buttonAscii.Add_Click( { onASCII });
 
-$button2.Location = New-Object System.Drawing.Point -ArgumentList @(81, 423);
-$button2.Name = "button2";
-$button2.Size = New-Object System.Drawing.Size -ArgumentList @(75, 23);
-$button2.Text = "UTF8";
-$button2.UseVisualStyleBackColor = $true;
-$button2.Add_Click( { onUTF8 });
+$buttonUtf8.Dock = [System.Windows.Forms.DockStyle]::Fill;
+$buttonUtf8.Name = "buttonUtf8";
+$buttonUtf8.Text = "UTF8";
+$buttonUtf8.UseVisualStyleBackColor = $true;
+$buttonUtf8.Add_Click( { onUTF8 });
 
-$button3.Location = New-Object System.Drawing.Point -ArgumentList @(159, 423);
-$button3.Name = "button3";
-$button3.Size = New-Object System.Drawing.Size -ArgumentList @(75, 23);
-$button3.Text = "Unicode";
-$button3.UseVisualStyleBackColor = $true;
-$button3.Add_Click( { onUnicode });
+$buttonUnicode.Dock = [System.Windows.Forms.DockStyle]::Fill;
+$buttonUnicode.Name = "buttonUnicode";
+$buttonUnicode.Text = "Unicode";
+$buttonUnicode.UseVisualStyleBackColor = $true;
+$buttonUnicode.Add_Click( { onUnicode });
+
+$buttonDnsA.Dock = [System.Windows.Forms.DockStyle]::Fill;
+$buttonDnsA.Name = "buttonDnsA";
+$buttonDnsA.Text = "DNS A";
+$buttonDnsA.UseVisualStyleBackColor = $true;
+$buttonDnsA.Add_Click( { onDnsA });
+
+$buttonDnsPTR.Dock = [System.Windows.Forms.DockStyle]::Fill;
+$buttonDnsPTR.Name = "buttonDnsPTR";
+$buttonDnsPTR.Text = "DNS PTR";
+$buttonDnsPTR.UseVisualStyleBackColor = $true;
+$buttonDnsPTR.Add_Click( { onDnsPTR });
+
+$buttonDnsCNAME.Dock = [System.Windows.Forms.DockStyle]::Fill;
+$buttonDnsCNAME.Name = "buttonDnsCNAME";
+$buttonDnsCNAME.Text = "DNS CNAME";
+$buttonDnsCNAME.UseVisualStyleBackColor = $true;
+$buttonDnsCNAME.Add_Click( { onDnsCNAME });
 
 $checkBox.Name = "checkBox";
 $checkBox.Text = "Word Wrap";
@@ -259,4 +341,4 @@ $label.TextAlign = [System.Drawing.ContentAlignment]::MiddleLeft;
 $Form.Controls.AddRange(@($tableLayoutPanel1))
 
 $Form.ShowDialog()
-
+#ENDREGION
