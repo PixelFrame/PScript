cb2911fb6c02c5cf8327a37fe1cfd36fbb5b51af
Pixel Frame <pm421@live.com>
2022-01-19 23:08:36 +0800

    New Security Descriptor printing

--- PsUtils/PsGUI/HexConverter.ps1
+++ PsUtils/PsGUI/HexConverter.ps1
@@ -576,22 +576,31 @@ function DoSdConvert
         $HexBytes
     )
 
-    $result = Invoke-CimMethod -ClassName Win32_SecurityDescriptorHelper -MethodName BinarySDToSDDL -Namespace 'ROOT/cimv2' -Arguments @{
-        BinarySD = $HexBytes
+    $SDDL = (Invoke-CimMethod -ClassName Win32_SecurityDescriptorHelper -MethodName BinarySDToSDDL -Namespace 'ROOT/cimv2' -Arguments @{ BinarySD = $HexBytes }).SDDL
+    $Win32SD = (Invoke-CimMethod -ClassName Win32_SecurityDescriptorHelper -MethodName BinarySDToWin32SD -Namespace 'ROOT/cimv2' -Arguments @{ BinarySD = $HexBytes }).Descriptor
+
+    $DACLString = ''
+    foreach ($DACL_ACE in $Win32SD.DACL) {
+        $_sdAce = New-Object SdAce -ArgumentList $DACL_ACE
+        $DACLString += "    $_sdAce`r`n"
     }
 
-    #TODO: ConvertFrom-SddlString result is based on local machine domain and some SIDs are not available. Will implement SDDL printing function.
-    $SD = ConvertFrom-SddlString $result.SDDL -Type FileSystemRights
+    $SACLString = ''
+    foreach ($SACL_ACE in $Win32SD.SACL)
+    {
+        $_sdAce = New-Object SdAce -ArgumentList $SACL_ACE
+        $SACLString += "    $_sdAce`r`n"
+    }
 
     $Output = @"
-SDDL: $($result.SDDL)
+SDDL: $($SDDL)
     
-Owner: $($SD.Owner)
-Group: $($SD.Group)
-DACL: 
-    $($SD.DiscretionaryAcl -join "`r`n    ")
+Owner: $($Win32SD.Owner.SIDString) ($($Win32SD.Owner.Domain)\$($Win32SD.Owner.Name))
+Group: $($Win32SD.Group.SIDString) ($($Win32SD.Group.Domain)\$($Win32SD.Group.Name))
+DACL:
+$DACLString
 SACL:
-    $($SD.SystemAcl)
+$SACLString
 "@
     return $Output
 }
@@ -976,5 +985,60 @@ enum DnsTypes
     CAA = 257
     TA = 32768
     DLV = 32769
+}
+
+[Flags()] 
+enum SdAceAccessMask {
+    FILE_READ_DATA              = 0x1;
+    FILE_WRITE_DATA             = 0x2;
+    FILE_APPEND_DATA            = 0x4;
+    FILE_READ_EA                = 0x8;
+    FILE_WRITE_EA               = 0x10;
+    FILE_EXECUTE_OR_TRAVERSE    = 0x20;
+    FILE_DELETE_CHILD           = 0x40;
+    FILE_READ_ATTRIBUTES        = 0x80;
+    FILE_WRITE_ATTRIBUTES       = 0x100;
+    DELETE                      = 0x10000;
+    READ_CONTROL                = 0x20000;
+    WRITE_DAC                   = 0x40000;
+    WRITE_OWNER                 = 0x80000;
+    SYNCHRONIZE                 = 0x100000;
+}
+
+[Flags()]
+enum SdAceFlags {
+    OBJECT_INHERIT_ACE          = 0x1;
+    CONTAINER_INHERIT_ACE       = 0x2;
+    NO_PROPAGATE_INHERIT_ACE    = 0x4;
+    INHERIT_ONLY_ACE            = 0x8;
+    INHERITED_ACE               = 0x10;
+    SUCCESSFUL_ACCESS_ACE_FLAG  = 0x40;
+    FAILED_ACCESS_ACE_FLAG      = 0x80;
+}
+
+enum SdAceType {
+    Allowed = 0;
+    Denied  = 1;
+    Audit   = 2;
+}
+
+class SdAce {
+    [SdAceAccessMask] $AccessMask;
+    [SdAceFlags] $AceFlags;
+    [SdAceType] $AceType;
+    [string] $Trustee;
+
+    SdAce($CimAce)
+    {
+        $this.AccessMask = $CimAce.AccessMask;
+        $this.AceFlags = $CimAce.AceFlags;
+        $this.AceType = $CimAce.AceType;
+        $this.Trustee = "$($CimAce.Trustee.SIDString) ($($CimAce.Trustee.Domain)\$($CimAce.Trustee.Name))"
+    }
+
+    [string] ToString()
+    {
+        return "$($this.AceType)  $($this.Trustee)  ($($this.AccessMask))  ($($this.AceFlags))"
+    }
 }
 #endregion
