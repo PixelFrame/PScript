d13987e78ec52e2452fa1df6de60605ac4605e31
Pixel Frame <pm421@live.com>
2023-02-12 16:13:24 +0800

    Added more robocopy options

--- PsUtils/ShellExtension/SyncFolderWith.ps1
+++ PsUtils/ShellExtension/SyncFolderWith.ps1
@@ -7,25 +7,27 @@ Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $InputForm = New-Object System.Windows.Forms.Form
-$InputForm.ClientSize = New-Object System.Drawing.Point(385, 125)
+$InputForm.ClientSize = New-Object System.Drawing.Point(390, 200)
+$InputForm.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
+$InputForm.MaximizeBox = $false
 $InputForm.text = 'Sync folder with ...' 
-$InputForm.TopMost = $false
+$InputForm.TopMost = $true
 
 $BtnOK = New-Object System.Windows.Forms.Button
 $BtnOK.Text = "OK"
 $BtnOK.Width = 60
-$BtnOK.Height = 20
-$BtnOK.Location = New-Object System.Drawing.Point(10, 100)
-$BtnOK.Font = New-Object System.Drawing.Font('Segoe UI', 10)
+$BtnOK.Height = 25
+$BtnOK.Location = New-Object System.Drawing.Point(10, 160)
+$BtnOK.Font = New-Object System.Drawing.Font('Segoe UI', 11)
 $BtnOK.DialogResult = [System.Windows.Forms.DialogResult]::OK
 $BtnOK.TabIndex = 2
 
 $BtnCancel = New-Object System.Windows.Forms.Button
 $BtnCancel.Text = "Cancel"
 $BtnCancel.Width = 60
-$BtnCancel.Height = 20
-$BtnCancel.Location = New-Object System.Drawing.Point(75, 100)
-$BtnCancel.Font = New-Object System.Drawing.Font('Segoe UI', 10)
+$BtnCancel.Height = 25
+$BtnCancel.Location = New-Object System.Drawing.Point(75, 160)
+$BtnCancel.Font = New-Object System.Drawing.Font('Segoe UI', 11)
 $BtnCancel.TabIndex = 3
 
 $tbSrcDir = New-Object System.Windows.Forms.TextBox
@@ -33,7 +35,7 @@ $tbSrcDir.Multiline = $false
 $tbSrcDir.Width = 300
 $tbSrcDir.Height = 30
 $tbSrcDir.Location = New-Object System.Drawing.Point(10, 5)
-$tbSrcDir.Font = New-Object System.Drawing.Font('Segoe UI', 10)
+$tbSrcDir.Font = New-Object System.Drawing.Font('Segoe UI', 11)
 $tbSrcDir.TabIndex = 0
 $tbSrcDir.ReadOnly = $true
 $tbSrcDir.Text = $SrcDir
@@ -43,38 +45,79 @@ $tbInput.Multiline = $false
 $tbInput.Width = 300
 $tbInput.Height = 30
 $tbInput.Location = New-Object System.Drawing.Point(10, 65)
-$tbInput.Font = New-Object System.Drawing.Font('Segoe UI', 10)
+$tbInput.Font = New-Object System.Drawing.Font('Segoe UI', 11)
 $tbInput.TabIndex = 0
 $tbInput.Text = $ENV:USERPROFILE
 
 $BtnBrowse = New-Object System.Windows.Forms.Button
 $BtnBrowse.Text = "Browse"
-$BtnBrowse.Width = 60
-$BtnBrowse.Height = 20
-$BtnBrowse.Location = New-Object System.Drawing.Point(320, 65)
-$BtnBrowse.Font = New-Object System.Drawing.Font('Segoe UI', 10)
+$BtnBrowse.Width = 65
+$BtnBrowse.Height = 25
+$BtnBrowse.Location = New-Object System.Drawing.Point(315, 65)
+$BtnBrowse.Font = New-Object System.Drawing.Font('Segoe UI', 11)
 $BtnBrowse.TabIndex = 1
 $BtnBrowse.Add_Click( { browseFolder })
 
 $rbFrom = New-Object System.Windows.Forms.RadioButton
 $rbFrom.Text = "From"
-$rbFrom.Checked = $true
+$rbFrom.Checked = $false
 $rbFrom.AutoSize = $true
-$rbFrom.Location =  New-Object System.Drawing.Point(10, 35)
-$rbFrom.Font = New-Object System.Drawing.Font('Segoe UI', 10)
+$rbFrom.Location = New-Object System.Drawing.Point(10, 35)
+$rbFrom.Font = New-Object System.Drawing.Font('Segoe UI', 11)
 
 $rbTo = New-Object System.Windows.Forms.RadioButton
 $rbTo.Text = "To"
-$rbTo.Checked = $false
+$rbTo.Checked = $true
 $rbTo.AutoSize = $true
-$rbTo.Location =  New-Object System.Drawing.Point(70, 35)
-$rbTo.Font = New-Object System.Drawing.Font('Segoe UI', 10)
+$rbTo.Location = New-Object System.Drawing.Point(70, 35)
+$rbTo.Font = New-Object System.Drawing.Font('Segoe UI', 11)
 
-function browseFolder {
+$tbFilter = New-Object System.Windows.Forms.TextBox
+$tbFilter.Multiline = $false
+$tbFilter.Width = 300
+$tbFilter.Height = 30
+$tbFilter.Location = New-Object System.Drawing.Point(10, 95)
+$tbFilter.Font = New-Object System.Drawing.Font('Segoe UI', 11)
+$tbFilter.Text = '*.*'
+
+$cbxRecusive = New-Object System.Windows.Forms.CheckBox
+$cbxRecusive.Text = "Recursive"
+$cbxRecusive.Checked = $true
+$cbxRecusive.AutoSize = $true
+$cbxRecusive.Location = New-Object System.Drawing.Point(10, 125)
+$cbxRecusive.Font = New-Object System.Drawing.Font('Segoe UI', 11)
+
+$cbxSec = New-Object System.Windows.Forms.CheckBox
+$cbxSec.Text = "Copy SEC Info"
+$cbxSec.Checked = $true
+$cbxSec.AutoSize = $true
+$cbxSec.Location = New-Object System.Drawing.Point(100, 125)
+$cbxSec.Font = New-Object System.Drawing.Font('Segoe UI', 11)
+
+$cbxMon = New-Object System.Windows.Forms.CheckBox
+$cbxMon.Text = "Montoring"
+$cbxMon.Checked = $false
+$cbxMon.AutoSize = $true
+$cbxMon.Location = New-Object System.Drawing.Point(220, 125)
+$cbxMon.Font = New-Object System.Drawing.Font('Segoe UI', 11)
+$cbxMon.Add_CheckedChanged( { $numUpDown.Enabled = $cbxMon.Checked })
+
+$numUpDown = New-Object System.Windows.Forms.NumericUpDown
+$numUpDown.Width = 65
+$numUpDown.Height = 30
+$numUpDown.Location = New-Object System.Drawing.Point(316, 125)
+$numUpDown.Font = New-Object System.Drawing.Font('Segoe UI', 11)
+$numUpDown.Minimum = 1
+$numUpDown.Maximum = 1440 # 60min x 24hr
+$numUpDown.Value = 10
+$numUpDown.Enabled = $false
+
+function browseFolder
+{
     param ( )
     $fbd = New-Object System.Windows.Forms.FolderBrowserDialog
     $result = $fbd.ShowDialog()
-    if($result -eq [System.Windows.Forms.DialogResult]::OK)
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK)
     {
         $tbInput.Text = $fbd.SelectedPath
     }
@@ -82,13 +125,16 @@ function browseFolder {
 
 $InputForm.AcceptButton = $BtnOK
 $InputForm.CancelButton = $BtnCancel
-$InputForm.Controls.AddRange(@($BtnOK, $BtnCancel, $tbInput, $tbSrcDir, $BtnBrowse, $rbFrom, $rbTo))
+$InputForm.Controls.AddRange(@($BtnOK, $BtnCancel, $tbInput, $tbSrcDir, $BtnBrowse, $rbFrom, $rbTo, $tbFilter, $cbxRecusive, $cbxSec, $cbxMon, $numUpDown))
 $dlgResult = $InputForm.ShowDialog()
 
 if ($dlgResult -eq [System.Windows.Forms.DialogResult]::OK)
 {
-    if ($rbFrom.Checked) { $param = "`"$($tbInput.Text)`" `"$SrcDir`" /MIR" }
-    else { $param = "`"$SrcDir`" `"$($tbInput.Text)`" /MIR" }
+    if ($rbFrom.Checked) { $param = "`"$($tbInput.Text)`" `"$SrcDir`" $($tbFilter.Text)" }
+    else { $param = "`"$SrcDir`" `"$($tbInput.Text)`" $($tbFilter.Text)" }
+    if ($cbxRecusive.Checked) { $param += " /E" }
+    if ($cbxSec.Checked) { $param += " /COPYALL" }
+    if ($cbxMon.Checked) { $param += " /MON:$($numUpDown.Value)" }
     $confirm = [System.Windows.Forms.MessageBox]::Show("Do you confirm to run command: `r`nrobocopy.exe $param", "Confirm", [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Warning)
     if ($confirm -eq [System.Windows.Forms.DialogResult]::Yes)
     {
