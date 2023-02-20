f45b1ce7b8ee18e31c4c72597fc9c109e2ad8a03
Pixel Frame <pm421@live.com>
2023-02-20 09:52:43 +0800

    Optimize sec copy and add elevation and logging

--- PsUtils/ShellExtension/RegisterSyncFolderWith.ps1
+++ PsUtils/ShellExtension/RegisterSyncFolderWith.ps1
@@ -1,3 +1,7 @@
+if (!(Test-Path "$($env:USERPROFILE)\Documents\WindowsPowerShell\Scripts"))
+{
+    [System.IO.Directory]::CreateDirectory("$($env:USERPROFILE)\Documents\WindowsPowerShell\Scripts")
+}
 $StubFile = "$($env:USERPROFILE)\Documents\WindowsPowerShell\Scripts\SyncFolderWith.ps1"
 Copy-Item -Path .\SyncFolderWith.ps1 -Destination $StubFile -Force
 
@@ -6,4 +10,4 @@ if (!(Test-Path $RegPath\'Background')) { (New-Item $RegPath\'Background' -Force
 if (!(Test-Path $RegPath\'Background\shell')) { (New-Item $RegPath\'Background\shell' -Force).Name }
 if (!(Test-Path $RegPath\'Background\shell\Sync this folder with')) { (New-Item $RegPath\'Background\shell\Sync this folder with' -Force).Name }
 if (!(Test-Path $RegPath\'Background\shell\Sync this folder with\command')) { (New-Item $RegPath\'Background\shell\Sync this folder with\command' -Force).Name }
-Set-ItemProperty -Path $RegPath\'Background\shell\Sync this folder with\command' -Name '(default)' -Value "powershell.exe -NoProfile -File `"$StubFile`" -SrcDir `"%V`"" -Force
+Set-ItemProperty -Path $RegPath\'Background\shell\Sync this folder with\command' -Name '(default)' -Value "pwsh.exe -NoProfile -File `"$StubFile`" -SrcDir `"%V`"" -Force
