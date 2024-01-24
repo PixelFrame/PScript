26a3a33558bbce8fcd491eca8f1a9aa06dfe8837
Pixel Frame <pm421@live.com>
2024-01-24 17:31:08 +0800

    Update SyncFolderWith

--- PsUtils/ShellExtension/SyncFolderWith.ps1
+++ PsUtils/ShellExtension/SyncFolderWith.ps1
@@ -11,7 +11,7 @@ Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $Form = New-Object System.Windows.Forms.Form
-$Form.ClientSize = New-Object System.Drawing.Point(480, 300)
+$Form.ClientSize = New-Object System.Drawing.Point(485, 285)
 $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
 $Form.MaximizeBox = $false
 $Form.Text = 'Folder Sync' 
@@ -24,7 +24,7 @@ $tbSource = New-Object System.Windows.Forms.TextBox
 $tbSource.Multiline = $false
 $tbSource.Width = 390
 $tbSource.Height = 30
-$tbSource.Location = New-Object System.Drawing.Point(5, 5)
+$tbSource.Location = New-Object System.Drawing.Point(10, 5)
 $tbSource.TabIndex = 0
 $tbSource.ReadOnly = $true
 $tbSource.Text = $SrcDir
@@ -34,23 +34,25 @@ $rbFrom = New-Object System.Windows.Forms.RadioButton
 $rbFrom.Text = "↑"
 $rbFrom.Checked = $false
 $rbFrom.AutoSize = $true
-$rbFrom.Location = New-Object System.Drawing.Point(60, 40)
+$rbFrom.Location = New-Object System.Drawing.Point(120, 40)
 $rbFrom.TabIndex = 1
+$rbFrom.Font = New-Object System.Drawing.Font('Segoe UI', 13)
 $Comps += $rbFrom
 
 $rbTo = New-Object System.Windows.Forms.RadioButton
 $rbTo.Text = "↓"
 $rbTo.Checked = $true
 $rbTo.AutoSize = $true
-$rbTo.Location = New-Object System.Drawing.Point(210, 40)
+$rbTo.Location = New-Object System.Drawing.Point(220, 40)
 $rbTo.TabIndex = 2
+$rbTo.Font = New-Object System.Drawing.Font('Segoe UI', 13)
 $Comps += $rbTo
 
 $tbDest = New-Object System.Windows.Forms.TextBox
 $tbDest.Multiline = $false
 $tbDest.Width = 390
 $tbDest.Height = 30
-$tbDest.Location = New-Object System.Drawing.Point(5, 75)
+$tbDest.Location = New-Object System.Drawing.Point(10, 75)
 $tbDest.TabIndex = 3
 $tbDest.Text = $ENV:USERPROFILE
 $Comps += $tbDest
@@ -59,7 +61,7 @@ $btnBrowse = New-Object System.Windows.Forms.Button
 $btnBrowse.Text = "Browse"
 $btnBrowse.Width = 70
 $btnBrowse.Height = 30
-$btnBrowse.Location = New-Object System.Drawing.Point(400, 75)
+$btnBrowse.Location = New-Object System.Drawing.Point(405, 75)
 $btnBrowse.TabIndex = 4
 $btnBrowse.Add_Click( { browseFolder })
 $Comps += $btnBrowse
@@ -68,7 +70,7 @@ $tbFilter = New-Object System.Windows.Forms.TextBox
 $tbFilter.Multiline = $false
 $tbFilter.Width = 465
 $tbFilter.Height = 30
-$tbFilter.Location = New-Object System.Drawing.Point(5, 110)
+$tbFilter.Location = New-Object System.Drawing.Point(10, 110)
 $tbFilter.TabIndex = 5
 $tbFilter.Text = "*"
 $Comps += $tbFilter
@@ -77,7 +79,7 @@ $cbxRecusive = New-Object System.Windows.Forms.CheckBox
 $cbxRecusive.Text = "Recursive"
 $cbxRecusive.Checked = $true
 $cbxRecusive.AutoSize = $true
-$cbxRecusive.Location = New-Object System.Drawing.Point(5, 145)
+$cbxRecusive.Location = New-Object System.Drawing.Point(10, 145)
 $cbxRecusive.TabIndex = 6
 $Comps += $cbxRecusive
 
@@ -85,7 +87,7 @@ $cbxPurge = New-Object System.Windows.Forms.CheckBox
 $cbxPurge.Text = "Purge"
 $cbxPurge.Checked = $true
 $cbxPurge.AutoSize = $true
-$cbxPurge.Location = New-Object System.Drawing.Point(130, 145)
+$cbxPurge.Location = New-Object System.Drawing.Point(135, 145)
 $cbxPurge.TabIndex = 7
 $Comps += $cbxPurge
 
@@ -93,7 +95,7 @@ $cbxCopyAcl = New-Object System.Windows.Forms.CheckBox
 $cbxCopyAcl.Text = "Copy ACL"
 $cbxCopyAcl.Checked = $false
 $cbxCopyAcl.AutoSize = $true
-$cbxCopyAcl.Location = New-Object System.Drawing.Point(255, 145)
+$cbxCopyAcl.Location = New-Object System.Drawing.Point(260, 145)
 $cbxCopyAcl.TabIndex = 8
 $Comps += $cbxCopyAcl
 
@@ -101,7 +103,7 @@ $cbxExHidden = New-Object System.Windows.Forms.CheckBox
 $cbxExHidden.Text = "Skip Hidden"
 $cbxExHidden.Checked = $true
 $cbxExHidden.AutoSize = $true
-$cbxExHidden.Location = New-Object System.Drawing.Point(5, 180)
+$cbxExHidden.Location = New-Object System.Drawing.Point(10, 180)
 $cbxExHidden.TabIndex = 9
 $Comps += $cbxExHidden
 
@@ -109,7 +111,7 @@ $cbxPreSym = New-Object System.Windows.Forms.CheckBox
 $cbxPreSym.Text = "Keep SymLink"
 $cbxPreSym.Checked = $false
 $cbxPreSym.AutoSize = $true
-$cbxPreSym.Location = New-Object System.Drawing.Point(130, 180)
+$cbxPreSym.Location = New-Object System.Drawing.Point(135, 180)
 $cbxPreSym.TabIndex = 10
 $Comps += $cbxPreSym
 
@@ -117,7 +119,7 @@ $cbxPreJunc = New-Object System.Windows.Forms.CheckBox
 $cbxPreJunc.Text = "Keep Junction"
 $cbxPreJunc.Checked = $false
 $cbxPreJunc.AutoSize = $true
-$cbxPreJunc.Location = New-Object System.Drawing.Point(255, 180)
+$cbxPreJunc.Location = New-Object System.Drawing.Point(260, 180)
 $cbxPreJunc.TabIndex = 11
 $Comps += $cbxPreJunc
 
@@ -125,7 +127,7 @@ $cbxElevate = New-Object System.Windows.Forms.CheckBox
 $cbxElevate.Text = "Run As Admin"
 $cbxElevate.Checked = $false
 $cbxElevate.AutoSize = $true
-$cbxElevate.Location = New-Object System.Drawing.Point(5, 215)
+$cbxElevate.Location = New-Object System.Drawing.Point(10, 215)
 $cbxElevate.TabIndex = 12
 $Comps += $cbxElevate
 
@@ -133,7 +135,7 @@ $cbxLogging = New-Object System.Windows.Forms.CheckBox
 $cbxLogging.Text = "Logging"
 $cbxLogging.Checked = $false
 $cbxLogging.AutoSize = $true
-$cbxLogging.Location = New-Object System.Drawing.Point(130, 215)
+$cbxLogging.Location = New-Object System.Drawing.Point(135, 215)
 $cbxLogging.TabIndex = 13
 $Comps += $cbxLogging
 
@@ -141,7 +143,7 @@ $cbxMonitor = New-Object System.Windows.Forms.CheckBox
 $cbxMonitor.Text = "Monitor"
 $cbxMonitor.Checked = $false
 $cbxMonitor.AutoSize = $true
-$cbxMonitor.Location = New-Object System.Drawing.Point(255, 215)
+$cbxMonitor.Location = New-Object System.Drawing.Point(260, 215)
 $cbxMonitor.Add_CheckedChanged( { $nudMonitor.Enabled = $cbxMonitor.Checked })
 $cbxMonitor.TabIndex = 14
 $Comps += $cbxMonitor
@@ -149,7 +151,7 @@ $Comps += $cbxMonitor
 $nudMonitor = New-Object System.Windows.Forms.NumericUpDown
 $nudMonitor.Width = 60
 $nudMonitor.Height = 30
-$nudMonitor.Location = New-Object System.Drawing.Point(380, 215)
+$nudMonitor.Location = New-Object System.Drawing.Point(385, 215)
 $nudMonitor.Minimum = 1
 $nudMonitor.Maximum = 1440 # 60min x 24hr
 $nudMonitor.Value = 10
@@ -161,7 +163,7 @@ $btnOK = New-Object System.Windows.Forms.Button
 $btnOK.Text = "OK"
 $btnOK.Width = 120
 $btnOK.Height = 30
-$btnOK.Location = New-Object System.Drawing.Point(5, 250)
+$btnOK.Location = New-Object System.Drawing.Point(10, 250)
 $btnOK.DialogResult = [System.Windows.Forms.DialogResult]::OK
 $btnOK.TabIndex = 16
 $Comps += $btnOK
@@ -170,7 +172,7 @@ $btnCancel = New-Object System.Windows.Forms.Button
 $btnCancel.Text = "Cancel"
 $btnCancel.Width = 120
 $btnCancel.Height = 30
-$btnCancel.Location = New-Object System.Drawing.Point(130, 250)
+$btnCancel.Location = New-Object System.Drawing.Point(135, 250)
 $btnCancel.TabIndex = 17
 $Comps += $btnCancel
 
@@ -185,15 +187,15 @@ function browseFolder
     }
 }
 
-$Form.AcceptButton = $BtnOK
-$Form.CancelButton = $BtnCancel
+$Form.AcceptButton = $btnOK
+$Form.CancelButton = $btnCancel
 $Form.Controls.AddRange($Comps)
 $dlgResult = $Form.ShowDialog()
 
 if ($dlgResult -eq [System.Windows.Forms.DialogResult]::OK)
 {
-    if ($rbFrom.Checked) { $param = "`"$($tbSource.Text)`" `"$SrcDir`" $($tbFilter.Text)" }
-    else { $param = "`"$SrcDir`" `"$($tbSource.Text)`" $($tbFilter.Text)" }
+    if ($rbFrom.Checked) { $param = "`"$($tbDest.Text)`" `"$SrcDir`" $($tbFilter.Text)" }
+    else { $param = "`"$SrcDir`" `"$($tbDest.Text)`" $($tbFilter.Text)" }
     if ($cbxRecusive.Checked) { $param += " /E" }
     if ($cbxPurge.Checked) { $param += " /PURGE" }
     if ($cbxCopyAcl.Checked) { $param += " /SEC" }
