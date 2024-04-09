9740f461e8e3119590f4b216fc9931c16d92492c
Pixel Frame <pm421@live.com>
2024-04-09 22:33:26 +0800

    Add LDAP ETW to pcap

---
+++ Workplace/ldapetw2pcap.ps1
@@ -0,0 +1,65 @@
+# Converts data in LDAP client trace to Wireshark ready HEX dump
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string] $LdapEtw,
+
+    [switch] $DecryptedOnly
+)
+
+$EtwLineRegex = '\[\d+\][\dA-F]{4}\.[\dA-F]{4}::(?<Time>\u200e\d{4}\u200e-\u200e\d{2}\u200e-\u200e\d{2} \d{2}:\d{2}:\d{2}\.\d{3}) \[Microsoft-Windows-LDAP-Client\](?<Message>.*)'
+$HexDumpRegex = '(?<HEX>[\da-f]{2} )+ (?<ASCII>.+)'
+
+$BeginDataSectionMark = 'Data (sent|received) on connection'
+$EndDataSectionMark = 'End of (send|receive) buffer.'
+$BeginDecryptedSectionMark = 'Unencrypted dump of Data (sent|received) on connection'
+$EndDecryptedSectionMark = 'End of Unencrypted dump of (send|receive) buffer.'
+
+$LdapEtwContent = Get-Content $LdapEtw
+$Output = $LdapEtw + '.hex'
+$Index = 0
+$IsInHexSection = $false
+$Progress = 0
+$CurrentDirection = ''
+
+Set-Content -Value ([string]::Empty) -LiteralPath $Output -NoNewline
+foreach ($line in $LdapEtwContent)
+{
+    Write-Progress -PercentComplete ($Progress++ / $LdapEtwContent.Count * 100) -Status "Line $Progress/$($LdapEtwContent.Count)" -Activity 'Processing file'
+    $RegMatch = [Regex]::Matches($line, $EtwLineRegex)
+    if ($null -ne $RegMatch)
+    {
+        $Message = ($RegMatch.Groups | Where-Object { $_.Name -eq 'Message' }).Value
+        if ($IsInHexSection)
+        {
+            if ($Message -match $EndDecryptedSectionMark -or (!$DecryptedOnly -and ($Message -match $EndDataSectionMark)) )
+            {
+                Out-File -Append -Encoding ascii -LiteralPath $Output -InputObject ([string]::Empty)
+                $IsInHexSection = $false
+                continue
+            }
+            $HexMatch = [Regex]::Matches($Message, $HexDumpRegex)
+            if ($null -ne $HexMatch)
+            {
+                $HexCaptures = ($HexMatch.Groups | Where-Object { $_.Name -eq 'HEX' }).Captures
+                $OutLine = $Index.ToString('X8') + '  ' + (-join $HexCaptures) + [System.Environment]::NewLine
+                $OutLine | Out-File -Append -Encoding ascii -LiteralPath $Output -NoNewline
+                $Index += $HexCaptures.Count
+            }
+        }
+        else
+        {
+            if ($Message -match $BeginDecryptedSectionMark -or (!$DecryptedOnly -and ($Message -match $BeginDataSectionMark)) )
+            {
+                $Time = ($RegMatch.Groups | Where-Object { $_.Name -eq 'Time' }).Value -replace '\u200e'
+                if ($Message.Contains('sent')) { $CurrentDirection = 'O' }
+                else { $CurrentDirection = 'I' }
+                $IsInHexSection = $true
+                $Index = 0
+                Out-File -Append -Encoding ascii -LiteralPath $Output -InputObject "$CurrentDirection $Time"
+                continue
+            }
+        }
+    }
+}
