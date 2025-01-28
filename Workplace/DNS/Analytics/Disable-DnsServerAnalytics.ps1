2901c5c2df4f0c397366807e293868925c916be1
Pixel Frame <pm421@live.com>
2025-01-28 08:39:43 +0800

    Cleanup

--- Workplace/DNS/Analytics/Disable-DnsServerAnalytics.ps1
+++
@@ -1,9 +0,0 @@
-[CmdletBinding()]
-param (
-)
-
-$log = New-Object System.Diagnostics.Eventing.Reader.EventLogConfiguration 'Microsoft-Windows-DNSServer/Analytical'
-$log.IsEnabled = $false
-$log.SaveChanges()
-
-Get-WinEvent -ListLog 'Microsoft-Windows-DNSServer/Analytical' | Format-List *
